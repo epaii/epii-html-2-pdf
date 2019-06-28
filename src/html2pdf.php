4bcbcd8ca107f39076a49488eba755dbbd60ebc6
MrRen <543169072@qq.com>
2019-06-28 14:15:47 +0800

    init

--- src/html2pdf.php
+++ src/html2pdf.php
@@ -124,7 +124,7 @@ class html2pdf
         curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
         //执行命令
         $data = curl_exec($curl);
-         var_dump($data);
+       //  var_dump($data);
         //关闭URL请求
         curl_close($curl);
         if (!$data) return false;
